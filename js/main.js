e753bd81c8d6e571dc1580e3f8bbafd3746a60af
pluusultra <ultracrackerrr@gmail.com>
2024-08-19 11:25:38 +0700

    add header and section top

---
+++ js/main.js
@@ -0,0 +1,14 @@
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+
+  effect: "fade",
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
